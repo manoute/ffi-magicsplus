e2cc63a15d7d424582a03f22d38f2216ad4e2a37
Mathieu Deslandes <math.deslandes@gmail.com>
2011-02-15 17:50:25 +0100

    ffi dependency with rubygem

--- tasks/gem.rb
+++ tasks/gem.rb
@@ -3,11 +3,12 @@ begin
 
   Echoe.new('ffi-magics++', '0.1.1') do |p|
     p.description    = "Ruby wrapper for magics++ using ffi"
-    p.url            = ""
+    p.url            = "https://github.com/manoute/ffi-magicsplus"
     p.author         = "Mathieu Deslandes"
     p.email          = "math.deslandes @nospam@ gmail.com"
     p.ignore_pattern = ["tmp/*", "script/*"]
     p.development_dependencies = ['ffi']
+    p.runtime_dependencies = ['ffi']
   end
 rescue LoadError => e
   p "e.message"
